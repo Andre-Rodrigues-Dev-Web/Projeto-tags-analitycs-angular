6ffcc536860216a9384ba0726da50c653e7ea18e
André Rodrigues <79713707+Andre-Rodrigues-Dev-Web@users.noreply.github.com>
2022-07-01 00:15:31 -0300

    add files

--- aplicacao-ga/src/app/app.module.ts
+++ aplicacao-ga/src/app/app.module.ts
@@ -4,12 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AboutComponent } from './about/about.component';
-import { GoogleTagManagerModule } from 'angular-google-tag-manager';
-import { GoogleAnalyticsService } from './services/google-analytics.service';
 @NgModule({
   declarations: [AppComponent, AboutComponent],
   imports: [BrowserModule, AppRoutingModule],
-  providers: [{ provide: 'googleTagManagerId', useValue: 'GTM-PV8586C' }],
+  providers: [{ provide: 'googleTagManagerId', useValue: 'GTM-T4F37NX' }],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
